e640fdb90884c2a290b28fb66d7a2ca646c310bb
katrinLuna <kate.nik2017@yandex.ru>
2019-12-16 19:05:07 +0300

    add limit for drag area

--- js/dialog.js
+++ js/dialog.js
@@ -11,13 +11,34 @@ setupElement.addEventListener('mousedown', function (evt) {
 
     var shiftCoordX = evtMove.clientX - startCoordX;
     var shiftCoordY = evtMove.clientY - startCoordY;
-    var bodyWidth = document.querySelector('body').offsetWidth / 2;
+    var bodyWidth = document.querySelector('body').offsetWidth;
+    var bodyHeight = document.querySelector('body').offsetHeight;
+    var setupElementHeigth = setupElement.offsetHeight;
+    var setupElementWidth = setupElement.offsetWidth;
+    var maxPageHeight = Math.max(bodyHeight, window.innerHeight);
 
     startCoordX = evtMove.clientX;
     startCoordY = evtMove.clientY;
 
-    setupElement.style.top = setupElement.offsetTop + shiftCoordY + 'px';
-    setupElement.style.left = setupElement.offsetLeft + shiftCoordX + 'px';
+    var newCoordY = setupElement.offsetTop + shiftCoordY;
+    var newCoordX = setupElement.offsetLeft + shiftCoordX;
+
+    if ((newCoordY + setupElementHeigth) > maxPageHeight) {
+      setupElement.style.top = (maxPageHeight - setupElementHeigth) + 'px';
+    } else if (newCoordY < 0) {
+      setupElement.style.top = 0;
+    } else {
+      setupElement.style.top = newCoordY + 'px';
+    }
+
+    if ((newCoordX + (setupElementWidth * 0.5)) > bodyWidth) {
+      setupElement.style.left = (bodyWidth - (setupElementWidth * 0.5)) + 'px';
+    } else if ((newCoordX - (setupElementWidth * 0.5)) < 0) {
+      setupElement.style.left = setupElementWidth * 0.5 + 'px';
+    } else {
+      setupElement.style.left = newCoordX + 'px';
+    }
+
 
   };
 
