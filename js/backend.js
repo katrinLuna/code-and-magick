cab74d5826067849e11a730ecb68eb7a1ee1957a
katrinLuna <kate.nik2017@yandex.ru>
2020-01-22 18:24:47 +0300

    fix download link

--- js/backend.js
+++ js/backend.js
@@ -1,13 +1,14 @@
 'use strict';
 
 (function () {
-  const DOWMLOAD_URL = 'https://js.dump.academy/code-and-magick/daa';
+  const DOWMLOAD_URL = 'https://js.dump.academy/code-and-magick/data';
   const SUCESS_SERVER_CODE = 200;
 
   window.backend = {
     load: function () {
-      return fetch(DOWMLOAD_URL)
+      var data = fetch(DOWMLOAD_URL)
         .then(data => data.json());
+      return data;
     },
     save: 2
   }
